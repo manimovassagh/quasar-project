995b8218dc3b43b22ab8fdf5ba7484a822b7c976
Mani Movassagh <mani.mgh@gmail.com>
2022-10-26 02:10:50 +0200

    dialog

---
+++ src/components/DialogSample.vue
@@ -0,0 +1,37 @@
+<!-- eslint-disable vue/no-mutating-props -->
+<template>
+  <q-dialog v-model="dialog" ref="log">
+    <q-card>
+      <q-card-section class="row items-center">
+        <q-avatar icon="signal_wifi_off" color="primary" text-color="white" />
+        <span class="q-ml-sm"
+          >You are currently not connected to any network.</span
+        >
+      </q-card-section>
+      <q-card-actions align="right">
+        <q-btn
+          @click="$refs.log.hide()"
+          flat
+          label="Cancel"
+          color="primary"
+          v-bind="$attrs"
+        />
+        <q-btn
+          @click="
+            $emit('accept');
+            $refs.log.hide();
+          "
+          flat
+          label="Turn on Wifi"
+          color="primary"
+          v-close-popup
+        />
+      </q-card-actions>
+    </q-card>
+  </q-dialog>
+</template>
+
+<script setup>
+import { computed, ref } from "vue";
+const dialog = ref(true);
+</script>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>test</div>
+  <DialogSample @accept="logger"></DialogSample>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import DialogSample from "src/components/DialogSample.vue";
+const toggle = ref(true);
+function logger() {
+  console.log("check");
+  toggle.value = !toggle.value;
+}
+</script>
