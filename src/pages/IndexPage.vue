ab9389cb62cbba36d93a3536f9d6c6b55b2980eb
Mani Movassagh <mani.mgh@gmail.com>
2022-10-29 18:02:19 +0200

    validate

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,17 +1,33 @@
 <template>
   <div>test</div>
   <DialogSample @accept="logger"></DialogSample>
-  <q-input outlined v-model="text" label="Outlined" />
+  <q-input
+    :error="hasError"
+    :error-message="errorMeessage"
+    outlined
+    v-model="text"
+    label="Outlined"
+  />
   {{ text }}
 </template>
 
 <script setup>
 import { ref } from "vue";
 import DialogSample from "src/components/DialogSample.vue";
+import { watch } from "fs";
 const toggle = ref(true);
 const text = ref("");
+const hasError = ref(false);
+const errorMeessage = ref("");
 function logger() {
   console.log("check");
   toggle.value = !toggle.value;
 }
+
+watch(text, async (newtext, oldText) => {
+  if (newtext.length > 5) {
+    hasError.value = true;
+    errorMeessage.value = "content should be less than 5 charachter";
+  }
+});
 </script>
